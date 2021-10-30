bb67bc78117fc77ac994a57299da6de01902f38c
Kaleb Su <khzputrafsr@gmail.com>
2021-10-30 14:59:48 +0700

    Menambahkan file Lab1.py

---
+++ Luas_dan_Keliling_Lingkaran.py
@@ -0,0 +1,8 @@
+import math
+r=float(input("Masukkan Jari-jari:"))
+
+luas=math.pi*(r*r)
+keliling=2*math.pi*r
+
+print("Luas Lingkaran \t\t=",format(luas,'.2f'))
+print("Keliling LIngkaran\t=",format(keliling,'.2f'))
